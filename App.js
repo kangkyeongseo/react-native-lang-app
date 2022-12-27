438bea54b4ac84d913800d82cb7a5b7acbbd2f4c
kangkyeongseo <kks_big@naver.com>
2022-12-27 17:39:12 +0900

    Card Project

--- App.js
+++ App.js
@@ -1,65 +1,139 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components/native";
-import { Animated, PanResponder } from "react-native";
+import { Animated, PanResponder, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import icons from "./icons";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-`;
-const Box = styled.View`
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
+  background-color: #00a8ff;
 `;
 
-const AnimatedBox = Animated.createAnimatedComponent(Box);
+const CardContainer = styled.View`
+  flex: 2;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Card = styled(Animated.createAnimatedComponent(View))`
+  background-color: white;
+  width: 200px;
+  height: 300px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
+  position: absolute;
+`;
+
+const BtnContainer = styled.View`
+  flex-direction: row;
+  flex: 1;
+`;
+
+const Btn = styled.TouchableOpacity`
+  margin: 0 10px;
+`;
 
 export default function App() {
-  const position = useRef(
-    new Animated.ValueXY({
-      x: 0,
-      y: 0,
-    })
-  ).current;
-  const borderRadius = position.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: [100, 0],
+  // Values
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.Value(0)).current;
+  const rotation = position.interpolate({
+    inputRange: [-250, 250],
+    outputRange: ["-15deg", "15deg"],
+    extrapolate: "clamp",
   });
-  const bgColor = position.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
+  const secondSclae = position.interpolate({
+    inputRange: [-250, 0, 250],
+    outputRange: [1, 0.5, 1],
+    extrapolate: "clamp",
   });
+  // Animations
+  const onPressIn = Animated.spring(scale, {
+    toValue: 0.95,
+    useNativeDriver: true,
+  });
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
+  const goCenter = Animated.spring(position, {
+    toValue: 0,
+    useNativeDriver: true,
+  });
+  const goLeft = Animated.spring(position, {
+    toValue: -400,
+    tension: 5,
+    useNativeDriver: true,
+  });
+  const goRight = Animated.spring(position, {
+    toValue: 400,
+    tension: 5,
+    useNativeDriver: true,
+  });
+  // Pan Responders
   const panResponder = useRef(
     PanResponder.create({
       onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx }) => {
+        position.setValue(dx);
+      },
       onPanResponderGrant: () => {
-        position.setOffset({
-          x: position.x._value,
-          y: position.y._value,
-        });
+        onPressIn.start();
       },
-      onPanResponderMove: (_, { dx, dy }) => {
-        position.setValue({
-          x: dx,
-          y: dy,
-        });
-      },
-      onPanResponderRelease: () => {
-        position.flattenOffset();
+      onPanResponderRelease: (_, { dx }) => {
+        if (dx < -240) {
+          goLeft.start(onDismiss);
+        } else if (dx > 240) {
+          goRight.start(onDismiss);
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
       },
     })
   ).current;
+  // State
+  const [index, setIndex] = useState(0);
+  const onDismiss = () => {
+    position.setValue(0);
+    setIndex((prev) => prev + 1);
+  };
+  const checkPress = () => {
+    goLeft.start(onDismiss);
+  };
+  const closePress = () => {
+    goRight.start(onDismiss);
+  };
   return (
     <Container>
-      <AnimatedBox
-        {...panResponder.panHandlers}
-        style={{
-          transform: [...position.getTranslateTransform()],
-          backgroundColor: bgColor,
-          borderRadius,
-        }}
-      />
+      <CardContainer>
+        <Card style={{ transform: [{ scale: secondSclae }] }}>
+          <Ionicons name={icons[index + 1]} color="#192a56" size={98} />
+        </Card>
+        <Card
+          {...panResponder.panHandlers}
+          style={{
+            transform: [
+              { scale },
+              { translateX: position },
+              { rotateZ: rotation },
+            ],
+          }}
+        >
+          <Ionicons name={icons[index]} color="#192a56" size={98} />
+        </Card>
+      </CardContainer>
+      <BtnContainer>
+        <Btn onPress={checkPress}>
+          <Ionicons name="checkmark-circle" color="white" size={42} />
+        </Btn>
+        <Btn onPress={closePress}>
+          <Ionicons name="close-circle" color="white" size={42} />
+        </Btn>
+      </BtnContainer>
     </Container>
   );
 }
